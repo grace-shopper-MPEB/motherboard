e405889fcbdeb75698141a10578967415b7af5ab
petesamper <petesamper@gmail.com>
2020-03-02 16:06:27 -0500

    This commit adds the Products model.

---
+++ server/db/models/products.js
@@ -0,0 +1,58 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Products = db.define('product', {
+  genre: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validations: {
+      notEmpty: true
+    }
+  },
+
+  artist: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validations: {
+      notEmpty: true
+    }
+  },
+
+  albumTitle: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validations: {
+      notEmpty: true
+    }
+  },
+
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validations: {
+      notEmpty: true
+    }
+  },
+
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+
+  retailPrice: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+
+  SKU: {
+    type: Sequelize.STRING,
+    allowNull: true
+  },
+
+  stock: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+})
+
+module.exports = Products
