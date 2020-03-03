0d2406e134b1d0aba2ecaafa2f50887288f0188d
petesamper <petesamper@gmail.com>
2020-03-03 15:03:21 -0500

    This commit has an initial AllProducts component.

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,16 +1,43 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {getProducts} from '../store/products'
 
 export class AllProducts extends React.Component {
-  componentDidMount() {}
+  componentDidMount() {
+    this.props.getProducts()
+  }
 
   render() {
-    return <div>It works</div>
+    if (this.props.products) {
+      return (
+        <div>
+          {this.props.products.map(product => (
+            <div key={product.id}>
+              <img src={product.imgUrl} />
+              <div>{product.albumTitle}</div>
+              <div>{product.artist}</div>
+              <div>{product.genre}</div>
+              <div>{product.description}</div>
+              <div>{product.price}</div>
+            </div>
+          ))}
+        </div>
+      )
+    }
+    return <h1>All records out of stock. Come back soon!</h1>
   }
 }
 
-const mapStateToProps = state => {}
+const mapStateToProps = state => {
+  return {
+    products: state.products
+  }
+}
 
-const mapDispatchToProps = dispatch => {}
+const mapDispatchToProps = dispatch => {
+  return {
+    getProducts: () => dispatch(getProducts())
+  }
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(AllProducts)
