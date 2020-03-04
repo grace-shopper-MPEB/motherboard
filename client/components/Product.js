4514dc5142942b445a38cd7c42ed064ca2de7e83
Vosters <mike.r.vosters@gmail.com>
2020-03-04 16:42:41 -0500

    Streamlined some redux and components

--- client/components/Product.js
+++ client/components/Product.js
@@ -1,27 +1,22 @@
 import React from 'react'
-import {BrowserRouter as Router, Link} from 'react-router-dom'
-import {connect} from 'react-redux'
-import {deleteRobot} from '../redux/robots'
+import {Link} from 'react-router-dom'
 
 export const Product = props => {
   const product = props.product
 
   return (
     <div key={product.id} className="item">
-      <img src={product.imgUrl} className="product-info" />
-      <h3>{product.albumTitle}</h3>
-      <div>{product.artist}</div>
-      <div>{product.genre}</div>
-      <div>{product.price}</div>
-      {/* // { Add a button feature to buy record } */}
+      <Link to={`/products/${product.id}`}>
+        <img src={product.imgUrl} className="product-info all-images" />
+      </Link>
+      <Link className="all-album-title" to={`/products/${product.id}`}>
+        <h3>{product.albumTitle}</h3>
+      </Link>
+      <div className="all-artist">{product.artist}</div>
+      <div className="all-price">${product.price}</div>
+      <button className="buyButton" type="button">
+        Add to Cart
+      </button>
     </div>
   )
 }
-
-const mapDispatchToProps = dispatch => {
-  return {
-    delete: id => dispatch(deleteRobot(id))
-  }
-}
-
-export default connect(null, mapDispatchToProps)(Robot)
