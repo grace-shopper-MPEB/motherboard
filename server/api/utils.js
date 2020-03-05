3bdb349cfb688830049fdddc77c97be5b3e43348
Vosters <mike.r.vosters@gmail.com>
2020-03-05 14:54:22 -0500

    Added notes

--- server/api/utils.js
+++ server/api/utils.js
@@ -1,4 +1,6 @@
 const isAdmin = (req, res, next) => {
+  // NEED TO UPDATE CONDITIONAL TO NOT ALLOW USERS WITHOUT ADMIN PROPERTY
+  // RIGHT NOW THIS IS JUST REQUIRING LOGIN
   if (!req.user) {
     return next('Sorry, admins only!')
   }
