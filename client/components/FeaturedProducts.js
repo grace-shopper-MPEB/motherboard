d654c28fe3d1c74c21ef9dd85f5755477250d238
Bobby Nixon <rhanixon1@gmail.com>
2020-03-09 16:02:30 -0400

    changed navbar names

--- client/components/FeaturedProducts.js
+++ client/components/FeaturedProducts.js
@@ -6,32 +6,35 @@ export class FeaturedProducts extends React.Component {
 
   render() {
     let ids = []
+
     for (let i = 0; i < 3; i++) {
       ids.push(Math.floor(Math.random() * 500))
     }
+
     const products = this.props.allProducts
-    console.log('props', products, ids)
 
     return (
-      <div>
-        <h1>
-          This Weeks Featured Albums:
-          {products.map(product => (
-            <div key={product.id}>
-              {product.id === ids[0] ||
-              product.id === ids[1] ||
-              product.id === ids[2] ? (
-                <Link to={`/products/${product.id}`}>
-                  <img
-                    src={product.imgUrl}
-                    className="product-info all-images"
-                    id="product-img"
-                  />
-                </Link>
-              ) : null}
-            </div>
-          ))}
-        </h1>
+      <div className="all-products-container">
+        <div className="all-products">
+          <h1>
+            This Weeks Featured Albums:
+            {products.map(product => (
+              <div key={product.id}>
+                {product.id === ids[0] ||
+                product.id === ids[1] ||
+                product.id === ids[2] ? (
+                  <Link to={`/products/${product.id}`}>
+                    <img
+                      src={product.imgUrl}
+                      className="product-info all-images"
+                      id="product-img"
+                    />
+                  </Link>
+                ) : null}
+              </div>
+            ))}
+          </h1>
+        </div>
       </div>
     )
   }
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -23,9 +23,9 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       {isLoggedIn ? (
         <div>
           <Link to="/home">Home</Link>
-          <Link to="/products">Products</Link>
+          <Link to="/products">All Records</Link>
+          <Link to="/products/featured">Featured Records</Link>
           <Link to="/users">Users</Link>
-          <Link to="/products/featured">Featured Albums</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
