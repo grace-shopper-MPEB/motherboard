4f66ed09caad2659ef1118d14e5f096d2b7b47f8
Bobby Nixon <rhanixon1@gmail.com>
2020-03-10 19:53:48 -0400

    filtering works now just need to handle on sumit instead of chahge

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -11,9 +11,11 @@ class AllProducts extends React.Component {
     super()
     this.handleClick = this.handleClick.bind(this)
     this.state = {
-      loading: true
+      loading: true,
+      search: ''
     }
   }
+
   componentDidMount() {
     this.setState({loading: false})
     this.props.fetchProducts()
@@ -23,6 +25,11 @@ class AllProducts extends React.Component {
     this.props.addToCart(product.id)
     toast.success('Added to Cart!')
     this.props.incrementPopularity(product)
+  }
+
+  updateSearch(event) {
+    this.setState({search: event.target.value})
+    console.log(this.state.search)
   }
 
   render() {
@@ -35,24 +42,39 @@ class AllProducts extends React.Component {
         </div>
       )
     }
+
+    let filteredProducts = this.props.allProducts.filter(product => {
+      return product.albumTitle.indexOf(this.state.search) !== -1
+    })
+
     const products = this.props.allProducts
+    console.log(filteredProducts)
     const user = this.props.user
+
     if (products) {
       return (
-        <div className="all-products-container">
-          <div className="all-products">
-            {products.map(product => (
-              <div key={product.id}>
-                <Product product={product} />
-                <button
-                  onClick={() => this.handleClick(product)}
-                  className="all buyButton"
-                  type="button"
-                >
-                  Add to Cart
-                </button>
-              </div>
-            ))}
+        <div>
+          <input
+            type="text"
+            placeholder="Search"
+            value={this.state.search}
+            onChange={this.updateSearch.bind(this)}
+          />
+          <div className="all-products-container">
+            <div className="all-products">
+              {filteredProducts.map(product => (
+                <div key={product.id}>
+                  <Product product={product} />
+                  <button
+                    onClick={() => this.handleClick(product)}
+                    className="all buyButton"
+                    type="button"
+                  >
+                    Add to Cart
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       )
